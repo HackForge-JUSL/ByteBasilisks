3504f9d24df5ab19029212b9a58c8882b1cf9262
Aindree Chatterjee <68018372+aindree-2005@users.noreply.github.com>
2024-03-16 23:37:11 +0530

    routing

--- app.py
+++ app.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-  
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
+
+@app.route('/assignments')
+def assignments():
+    return render_template('assignments.html')
+
+@app.route('/class-tests')
+def class_tests():
+    return render_template('class_tests.html')
+
+@app.route('/groups')
+def groups():
+    return render_template('groups.html')
+
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
